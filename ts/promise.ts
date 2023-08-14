6605b294830bfb59cbd842d5d0faa467d370a843
yiaihugongyuan <42810916@qq.com>
2023-08-14 11:36:38 +0800

    ==

--- ts/promise.ts
+++ ts/promise.ts
@@ -100,17 +100,21 @@ const m = new promiseMsg()
 // );
 
 // 案例 2：真实的 Promise 试验
-const p = new Promise(
-    (resolve, reject) => {
-        m.addMsg('案例 2：构造函数调用了我')
+m.addMsg('案例2：定义执行器函数，这个函数将会作为参数传递给Promise的构造函数，并被构造函数调用。')
+const executor = (resolve:(value:any)=>void,reject:(value:any)=>void) => {
+        m.addMsg('案例 2：executor函数输出：构造函数调用了我')
         resolve('案例 2：传递给 then 方法的值')
-        m.addMsg('案例 2：resolve方法调用完成')
-    }
-)
-m.addMsg('案例 2：准备调用 then 方法，传递状态处理函数')
+        m.addMsg('案例 2：executor函数输出：resolve方法调用完成')
+}
+const onFulfilled = ()=>{}
+const p = new Promise(executor)
+m.addMsg('案例 2：准备调用 then 方法，传递两个状态状态处理函数')
 p.then(value => {
     m.addMsg('案例 2：then方法传递给 promise 的方法被调用')
     m.addMsg('案例 2：得到了 resolve 的值：' + value)
 })
 m.addMsg('案例 2：其实我是先于 then 方法被调用的，说明通过 then 方法传递给 promise 的方法是异步的')
+
+
+
 m.showMsg()
